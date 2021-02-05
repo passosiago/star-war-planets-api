068fc7a59b0541f1de554e4a12235b91f0466578
Iago Passos <passosiago@gmail.com>
2021-02-05 13:50:53 -0300

    adding routes

--- api/routes/planets_routes.py
+++ api/routes/planets_routes.py
@@ -1,7 +1,38 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, Response
+from flask.json import jsonify
+from api.services.planet_services import PlanetService
 
 planet_blueprint = Blueprint("planet_blueprint", __name__)
 
+service = PlanetService()
+
+
 @planet_blueprint.route("/planets",  methods=['GET'])
 def planets():
-    return 'planets'
+    planets = service.get_planets()
+    return planets, 200
+
+
+@planet_blueprint.route("/planets/<string:id>",  methods=['GET'])
+def planet_by_id(id):
+    planet = service.get_planet_by_id(id)
+    return planet, 200
+
+
+@planet_blueprint.route("/planets/name/<string:name>",  methods=['GET'])
+def planet_by_name(name):
+    planet = service.get_planet_by_name(name)
+    return planet, 200
+
+
+@planet_blueprint.route("/planets/<string:id>",  methods=['DELETE'])
+def delete(id):
+    service.delete_planet_by_id(id)
+    return jsonify(), 200
+
+
+@planet_blueprint.route("/planets",  methods=['POST'])
+def add_planet():
+    planet = request.json
+    planet_id = service.create_planet(planet)
+    return jsonify(planet_id), 200
